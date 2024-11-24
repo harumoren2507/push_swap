f9919881fedfbe3d68c69590fd85460dd937b431
retoriya <kuroshiro0725@gmail.com>
2024-11-24 17:10:48 +0900

    first_commit

---
+++ push_swap.c
@@ -0,0 +1,20 @@
+int main(int argc, char *argv[])
+{
+  t_list **stack_a;
+  t_list **stack_b;
+
+  if (argc < 2)
+    return (1);
+  ft_check_args(argc, argv);
+  //wポインタの意味がまだ分かってない。
+  stack_a = malloc();
+  stack_a = malloc();
+
+  *stack_a = NULL;
+  *stack_b = NULL;
+  init_stack(stack_a, argc, argv);//たぶん、argvの数字群をstack_aにぶち込む。
+  if (//stack_aがそーとされていた場合の処理)
+  {
+  }
+  //sort;
+}
