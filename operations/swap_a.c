50f821ce4e9b6dc2d36d0831574f52ac8996302b
harumoren2507 <kuroshiro0725@gmail.com>
2024-12-09 23:24:24 +0900

    sa

--- operations/swap_a.c
+++ operations/swap_a.c
@@ -1,13 +1,24 @@
 #include "includes/push_swap.h"
 
-void	sa(t_stack *a)
+void	ft_sa(t_stack **stack_a)
 {
-	int	tmp;
-	tmp = 0;
+	t_stack *temp;
 
-	tmp = a->value;
-	a->value = a->next->value;
-	a->next->value = tmp;
+	if (!*stack_a || !(*stack_a->next)
+		return ;
+	temp = *stack_a;
+	*stack_a = (*stack_a)->next;
+	temp->next = (*stack_a)->next;
+	*stack_a->next = temp;
+	write(1, "sa\n", 3);
 }
+	/*
+
+	a[] = {1 2 3}
+	temp = *stack_a; temp = 1を指す
+	*stack_a = (stack_a)->next; つまり、stack_aの先頭は2になる.
+	temp->next = (stack_a)->next;   temp->next = 3; 2を指していたところ、1の次は3になる。
+	*stack_a->next(3) = temp; 2の次は1になる。
+	*/
 
 
